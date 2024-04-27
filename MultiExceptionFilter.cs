7cd4e2064441a4c9553f8189a213b8856212cf84
bishnu karki <bishnukarki1988@gmail.com>
2024-04-27 15:13:44 -0700

    multiple exception filter

---
+++ MultiExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    public class MultiExceptionFilter
+    {
+        public bool MultiCatchOperation()
+        {
+            bool success = true;
+
+            try
+            {
+                // operation that could throw multiple types of exception
+            }
+            catch (Exception ex) when ((ex is NullReferenceException) ||
+                                        (ex is ArgumentNullException) ||
+                                        (ex is ArgumentOutOfRangeException))
+            {
+                success = false;
+            }
+
+            return success;
+        }
+    }
+}
